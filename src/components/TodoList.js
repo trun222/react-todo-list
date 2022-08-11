5b011f46516ced5271cdb557773cc7c41fdc9c97
trun222 <thomasunderwoodii@gmail.com>
2022-08-11 10:23:59 -0400

    Clean up Code

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,60 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import './TodoList.css';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuidv4 from 'uuid/v4';
+import React, { useState, useEffect } from "react";
+import "./TodoList.css";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import uuidv4 from "uuid/v4";
 
 function TodoList() {
-  const [todo, setTodo] = useState('');
+  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
   const [list, setList] = useState([]);
-  const [todoCount, setTodoCount] = useState('');
-  const [color, setColor] = useState('#d3d3d3');
+  const [todoCount, setTodoCount] = useState("");
+  const [color, setColor] = useState("#d3d3d3");
 
   useEffect(() => {
-    setList(todos.map((item, index) => {
-      return (
-        <CSSTransition key={item.id} in={true} timeout={500} classNames="move">
-          <li key={index.id} style={{ background: index % 2 === 1 ? color : '#ffff'}}>
-            <Grid container spacing={8}>
-              <Grid className="background" item xs={10}>
-                <span>{index+1}. {item.text}</span>
+    setList(
+      todos.map((item, index) => {
+        return (
+          <CSSTransition
+            key={item.id}
+            in={true}
+            timeout={500}
+            classNames="move"
+          >
+            <li
+              key={index.id}
+              style={{ background: index % 2 === 1 ? color : "#ffff" }}
+            >
+              <Grid container spacing={8}>
+                <Grid className="background" item xs={10}>
+                  <span>
+                    {index + 1}. {item.text}
+                  </span>
+                </Grid>
+                <Grid item xs={2}>
+                  <IconButton
+                    color={"secondary"}
+                    aria-label="Delete"
+                    onClick={() => {
+                      handleRemoveTodo(index);
+                    }}
+                  >
+                    <DeleteIcon fontSize="small" />
+                  </IconButton>
+                </Grid>
               </Grid>
-              <Grid item xs={2}>
-                <IconButton color={"secondary"} aria-label="Delete" onClick={() => { handleRemoveTodo(index) }}>
-                  <DeleteIcon fontSize="small" />
-                </IconButton>
-              </Grid>
-            </Grid>
-          </li>
-        </CSSTransition>
-      );
-    }))
-    setTodoCount(list.length > 0 ? `Todo (${list.length})` : 'Todo');
+            </li>
+          </CSSTransition>
+        );
+      })
+    );
+    setTodoCount(list.length > 0 ? `Todo (${list.length})` : "Todo");
   }, [todos, todoCount, list]);
 
   // Handle when a Todo is 'added'
   function handleAddTodo(e) {
-    if(e.target.value) {
-      if(e.key === 'Enter') {
+    if (e.target.value) {
+      if (e.key === "Enter") {
         setTodos([{ text: e.target.value, id: uuidv4() }, ...todos]);
-        setTodo('');
+        setTodo("");
       }
     }
   }
 
   // Handle when a Todo is 'removed'
   function handleRemoveTodo(index) {
-    setTodos(todos.filter((todo, i) => { if(index !== i) { return todo } }));
+    setTodos(todos.filter((todo, i) => index !== i));
   }
 
   return (
     <div>
       <div className="color-picker">
-        <input type="color" onChange={(e) => { setColor(e.target.value) }} value={color}></input>
+        <input
+          type="color"
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+          value={color}
+        ></input>
       </div>
       <div className="todo-list-component">
         <div className="add-todo-container">
@@ -65,7 +89,9 @@ function TodoList() {
             className="add-todo"
             value={todo}
             onKeyPress={handleAddTodo}
-            onChange={(e) => { setTodo(e.target.value)  }}
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
           />
         </div>
         <div className="todo-list-container">
